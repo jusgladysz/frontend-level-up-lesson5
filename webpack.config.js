07d35fb73dfa367d2fa80f2a064ebb498485cc9f
Justyna Gładysz <jus.gladysz@gmail.com>
2020-04-12 19:34:31 +0200

    Add simple webpack config

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        filename: 'index.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+            template: './src/index.html',
+            filename: './index.html'
+        })
+    ]
+}
