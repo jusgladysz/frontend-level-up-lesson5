1297af699b9e26d1f0eb799440c91d7c057d07de
Justyna Gładysz <jus.gladysz@gmail.com>
2020-04-27 15:41:37 +0200

    Refactor module import

--- src/index.js
+++ src/index.js
@@ -1,13 +1,10 @@
 import './styles/reset.scss'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './styles/style.scss';
+import mainModule from './js/main-module';
 
 document.addEventListener('DOMContentLoaded', () => {
-    window.mainModule = import('./js/main-module');
-    window.mainModule.then(value => {
-        const mainModule = value.default;
-        mainModule.setCurrentYearToFooter();
-        mainModule.initLoadAllProducts();
-        mainModule.initScrollToTop();
-    });
-})
+    mainModule.setCurrentYearToFooter();
+    mainModule.initLoadAllProducts();
+    mainModule.initScrollToTop();
+});
