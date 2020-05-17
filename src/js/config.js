47777777fc87cab3085fbdbba6fa3455ab84b47a
Justyna Gładysz <jus.gladysz@gmail.com>
2020-05-17 18:54:15 +0200

    Update api key

--- src/js/config.js
+++ src/js/config.js
@@ -1,2 +1,2 @@
 export const API_HOST = 'asos2.p.rapidapi.com';
-export const API_KEY = '162c309a81msh0dd60bd3e839b88p13205cjsn7c3eb687a19c';
+export const API_KEY = 'fd0d8a8ea2msh00bfb8b4f8c51d0p1e85cdjsnd32e811260c9';
